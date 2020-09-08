5fbe64b04cabcaadbfdb91fff7f8d58bfe8015b0
C.J. May <doctormay6@gmail.com>
2020-09-07 23:41:04 -0500

    done building meet document, started building school document

--- ResultsService/app/milesplit.py
+++ ResultsService/app/milesplit.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import difflib
 import json
 from mongoengine import *
 from selenium import webdriver
@@ -16,8 +17,8 @@ class Result(EmbeddedDocument):
 class Meet(Document):
     name = StringField(required=True)
     date = DateField(required=True)
-    boysResults = ListField()
-    girlsResults = ListField()
+    boysResults = ListField(EmbeddedDocumentField(Result))
+    girlsResults = ListField(EmbeddedDocumentField(Result))
 
 
 class Athlete(EmbeddedDocument):
@@ -53,14 +54,16 @@ class MileSplit():
         # soup = BeautifulSoup(page.get_attribute("innerHTML"), "html.parser")
         soup = BeautifulSoup(self.driver.page_source, "html.parser")
         # get meet name and date
-        meet = soup.select("h1.meetName")[0]
-        # TODO: get meet date
-        # TODO: create meet document
+        meet = soup.select("h1.meetName")[0].get_text().strip()
+        date = soup.select("div.date")[0].find_all("time")[0].get_text().strip()
+        # create mongo meet document
+        meetDoc = Meet(name=meet, date=date, boysResults=[], girlsResults=[])
 
         # parse result data
         data = soup.find_all("table")[0]
         headers = data.find_all("thead")
         results = data.find_all("tbody")
+        # do for each 5k race
         for sectionNum in range(len(headers)):
             sectionTitle = headers[sectionNum].get_text().strip()
             sectionTitle = sectionTitle[:sectionTitle.index("\n")]
@@ -72,16 +75,53 @@ class MileSplit():
                 gender = "f"
             else:
                 continue
+            # parse every result from this race
             for finish in results[sectionNum].find_all("tr"):
                 place, name, grade, school, time, points = ((field.get_text() if "data-text" not in field.attrs else (field.get_text() if not field["data-text"] else field["data-text"])) for field in finish.find_all("td"))  # nice
                 result = Result(name=" ".join(name.split()),
                                 school=" ".join(school.split()),
                                 time=" ".join(time.split()))
-                # TODO: add result to meet according to gender
+                # add result to mongo meet document
+                if gender == "m":
+                    meetDoc.boysResults.append(result)
+                elif gender == "f":
+                    meetDoc.girlsResults.append(result)
+                else:
+                    raise Exception("Tried to add result without gender... How did this happen?")
+                # add school if not already in db
+                schoolExists = School.objects(name=school).count()
+                if not schoolExists:
+                    schoolDoc = School(name=school, classSize=self.getClass(gender, school), boys=[], girls=[])
+                else:
+                    schoolDoc = School.objects
 
-                # TODO: add school if not already in db
                 # TODO: add athlete according to gender if not already in db
                 # TODO: add meet to athlete's meets
                 print(json.loads(result.to_json()))
+        print(meet)
+        print(date)
 
-
+    def getClass(self, gender, schoolName):
+        if gender == "m":
+            list = "boy"
+        else:
+            list = "girl"
+        with open("static/"+gender+"ClassesFormatted.json", "r") as classesFile:
+            classes = json.load(classesFile)
+            # TODO: write this better when I am more awake
+            matches4a = difflib.get_close_matches(schoolName, classes["4A"], n=1, cutoff=.7)
+            matches3a = difflib.get_close_matches(schoolName, classes["3A"], n=1, cutoff=.7)
+            matches2a = difflib.get_close_matches(schoolName, classes["2A"], n=1, cutoff=.7)
+            matches1a = difflib.get_close_matches(schoolName, classes["1A"], n=1, cutoff=.7)
+            found = ""
+            if len(matches4a) > max(len(matches1a), len(matches2a), len(matches3a)):
+                found = "4A"
+            if len(matches3a) > max(len(matches1a), len(matches2a), len(matches4a)):
+                found = "3A"
+            if len(matches2a) > max(len(matches1a), len(matches3a), len(matches4a)):
+                found = "2A"
+            if len(matches1a) > max(len(matches2a), len(matches3a), len(matches4a)):
+                found = "1A"
+            if not found:
+                raise Exception("Could not match school " + schoolName + " to a class size. No match found or a match in multiple classes was found.")
+            return found
