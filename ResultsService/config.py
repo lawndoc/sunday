23659d39d21446119fdf1189374c350e377e8c38
C.J. May <doctormay6@gmail.com>
2021-09-12 12:39:30 -0500

    updated localdb name

--- ResultsService/config.py
+++ ResultsService/config.py
@@ -7,4 +7,4 @@ class Config(object):
     DB_PASS = os.environ.get("DB_PASS") or None
     DB_NAME = os.environ.get("DB_NAME") or None
     DB_URI = os.environ.get("DB_URI") or None
-    LOCALDB = "xcstats20"
+    LOCALDB = "xcstats21"
