19f7a40b4a3c4c31c4d9ab37379386717239d11e
C.J. May <doctormay6@gmail.com>
2021-09-20 19:00:07 -0500

    table is visible :,) smol victory

--- frontend/src/pages/Results.vue
+++ frontend/src/pages/Results.vue
@@ -1,15 +1,7 @@
 <template>
   <q-page class="q-pa-sm">
 
-    <tables-basic></tables-basic>
-
-    <table-actions class="q-mt-lg"></table-actions>
-
-    <table-dark-mode class="q-mt-lg"></table-dark-mode>
-
-    <table-progress class="q-mt-lg"></table-progress>
-
-    <table-custom-grid class="q-mt-lg"></table-custom-grid>
+    <results-table></results-table>
 
   </q-page>
 </template>
