359f76758d71faa384ad405eefdd03c3ba9dbe9e
C.J. May <doctormay6@gmail.com>
2021-09-20 19:58:51 -0500

    added csv export and fixed pagination

--- frontend/src/components/ResultsTable.vue
+++ frontend/src/components/ResultsTable.vue
@@ -6,13 +6,16 @@
       </div>
     </q-card-section>
     <q-card-section class="q-pa-none">
+      <!-- eslint-disable vue/no-v-model-argument -->
       <q-table
         title="Boys Varsity"
         :rows="data"
         :columns="columns"
         row-key="name"
         :filter="filter"
+        :pagination="pagination"
       >
+      <!-- eslint-enable -->
         <template v-slot:top-right>
           <q-input v-if="show_filter" filled borderless dense debounce="300" v-model="filter" placeholder="Search">
             <template v-slot:append>
@@ -21,6 +24,13 @@
           </q-input>
 
           <q-btn class="q-ml-sm" icon="filter_list" @click="show_filter=!show_filter" flat/>
+          <q-btn
+                color="primary"
+                icon-right="archive"
+                label="Export to csv"
+                no-caps
+                @click="exportTable"
+              />
         </template>
       </q-table>
     </q-card-section>
@@ -29,6 +39,25 @@
 
 <script>
 import {defineComponent, ref} from 'vue'
+import {exportFile} from 'quasar'
+
+function wrapCsvValue(val, formatFn) {
+  let formatted = formatFn !== void 0
+    ? formatFn(val)
+    : val
+  formatted = formatted === void 0 || formatted === null
+    ? ''
+    : String(formatted)
+  formatted = formatted.split('"').join('""')
+  /**
+   * Excel accepts \n and \r in strings, but some other CSV parsers do not
+   * Uncomment the next two lines to escape new lines
+   */
+  // .split('\n').join('\\n')
+  // .split('\r').join('\\r')
+  return `"${formatted}"`
+}
+
 const columns = [
   {
     name: 'name',
@@ -95,7 +124,34 @@ export default defineComponent({
       filter: ref(''),
       show_filter,
       data,
-      columns
+      columns,
+      pagination: {
+        rowsPerPage: 50
+      },
+
+      exportTable() {
+        // naive encoding to csv format
+        const content = [columns.map(col => wrapCsvValue(col.label))].concat(
+          data.map(row => columns.map(col => wrapCsvValue(
+            typeof col.field === 'function'
+              ? col.field(row)
+              : row[col.field === void 0 ? col.name : col.field],
+            col.format
+          )).join(','))
+        ).join('\r\n')
+        const status = exportFile(
+          'table-export.csv',
+          content,
+          'text/csv'
+        )
+        if (status !== true) {
+          $q.notify({
+            message: 'Browser denied file download...',
+            color: 'negative',
+            icon: 'warning'
+          })
+        }
+      }
     }
   }
 })
