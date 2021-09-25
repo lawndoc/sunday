57804ef497d1a713b1da031726be9745b527f264
C.J. May <doctormay6@gmail.com>
2021-09-25 17:53:20 -0500

    fixed pagination

--- results/api/routes.py
+++ results/api/routes.py
@@ -7,27 +7,34 @@ from flask import jsonify, request
 def getMeets():
     """ Return all Meet documents """
     page = int(request.args.get('page',1))
-    limit = int(request.args.get('limit',0))  # TODO: test to see if this sets no limit
-    meets = Meet.objects.exclude("boysResults", "girlsResults").paginate(page=page, per_page=limit)
+    limit = int(request.args.get('limit',0))
+    offset = (page-1) * limit
+    print(limit)
+    meets = Meet.objects.exclude("boysResults", "girlsResults").skip(offset).limit(limit)
     return jsonify(meets), 200
 
 @app.get("/meets/<id>")
 def getOneMeet(id: str):
     """ Return the specified Meet document """
     boysPage = int(request.args.get('boys-page',1))
-    boysLimit = int(request.args.get('boys-limit',0))  # TODO: test to see if this sets no limit
+    boysLimit = int(request.args.get('boys-limit',0))
+    boysOffset = (boysPage-1) * boysLimit
+    boysLast = boysOffset + boysLimit
     girlsPage = int(request.args.get('girls-page',1))
-    girlsLimit = int(request.args.get('girls-limit',0))  # TODO: test to see if this sets no limit
-    meet = Meet.objects.filter(id=id).fields(slice__boysResults=[boysPage-1, boysLimit],
-                                             slice__girlsResults=[girlsPage-1, girlsLimit]).first()
+    girlsLimit = int(request.args.get('girls-limit',0))
+    girlsOffset = (girlsPage-1) * girlsLimit
+    girlsLast = girlsOffset + girlsLimit
+    meet = Meet.objects.filter(id=id).fields(slice__boysResults=[boysOffset, boysLast],
+                                             slice__girlsResults=[girlsOffset, girlsLast]).first()
     return jsonify(meet), 200
 
 @app.get("/schools")
 def getSchools():
     """ Return all School documents """
     page = int(request.args.get('page',1))
-    limit = int(request.args.get('limit',0))  # TODO: test to see if this sets no limit
-    meets = School.objects.exclude("boys", "girls").paginate(page=page, per_page=limit)
+    limit = int(request.args.get('limit',0))
+    offset = (page-1) * limit
+    meets = School.objects.exclude("boys", "girls").skip(offset).limit(limit)
     return jsonify(meets), 200
 
 @app.get("/schools/<id>")
